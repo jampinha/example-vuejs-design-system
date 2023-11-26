b90f22755a755f85a2e10d29ec762ddd62fdacd5
Mario Bandeira <josemariobandeira@hotmail.com>
2023-11-26 00:29:31 -0300

    refactor: improve layout

--- src/components/layouts/MNavbar/MNavbar.vue
+++ src/components/layouts/MNavbar/MNavbar.vue
@@ -1,17 +1,20 @@
 <template>
 	<div class="m-navbar">
-		<div class="logo">
-			<m-image :logo="logo" />
-		</div>
-		<nav class="links">
-			<a href="#">Link</a>
-			<a href="#">Link</a>
-			<a href="#">Link</a>
-		</nav>
+		<m-container>
+			<div class="logo">
+				<m-image :logo="logo" />
+			</div>
+			<nav class="links">
+				<a href="#">Início</a>
+				<a href="#">Sobre</a>
+				<a href="#">Projetos</a>
+			</nav>
+		</m-container>
 	</div>
 </template>
 
 <script lang="ts" setup>
+import MContainer from '@/components/shared/MContainer/MContainer.vue';
 import MImage from '@/components/shared/MImage/MImage.vue';
 
 defineOptions({
@@ -26,33 +29,36 @@ const logo = new URL('@/assets/images/meiuca-logo.png', import.meta.url).href;
 	background-color: var(--color-background);
 	position: relative;
 	height: 70px;
-	padding: 1rem;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
 
 	-webkit-box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.75);
 	-moz-box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.75);
 	box-shadow: 0px 2px 8px -1px rgba(0, 0, 0, 0.75);
 
-	> .logo {
-		align-self: flex-start;
-		width: 100px;
-		height: 100%;
-	}
-
-	> .links {
+	> .m-container {
 		display: flex;
-		gap: 16px;
+		align-items: center;
+		height: 100%;
+		gap: 3rem;
 
-		> a {
-			color: var(--color-inverse);
+		> .logo {
+			align-self: flex-start;
+			width: 100px;
+			height: 100%;
 		}
-	}
 
-	@media (prefers-color-scheme: light) {
-		> .logo img {
-			filter: invert(1);
+		> .links {
+			display: flex;
+			gap: 16px;
+
+			> a {
+				color: var(--color-inverse);
+			}
+		}
+
+		@media (prefers-color-scheme: light) {
+			> .logo img {
+				filter: invert(1);
+			}
 		}
 	}
 }
