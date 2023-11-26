a152c66cb3d8b05ae46ce13ba085fdc4a1d717fe
Mario Bandeira <josemariobandeira@hotmail.com>
2023-11-25 23:05:47 -0300

    feat: implement MNavbar component

--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
 import MContainer from '@/components/shared/MContainer/MContainer.vue';
+import MNavbar from '../layouts/MNavbar/MNavbar.vue';
 </script>
 
 <template>
+	<m-navbar> </m-navbar>
 	<main>
-		<m-container>
-			I'm the container component
-		</m-container>
+		<m-container> I'm the container component </m-container>
 	</main>
 </template>
