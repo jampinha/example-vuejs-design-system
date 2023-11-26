38eddf0e43bfd6eb48393f63d1dadd001627861f
Mario Bandeira <josemariobandeira@hotmail.com>
2023-11-25 21:34:51 -0300

    refactor: place MSplash in you own folder

--- src/components/layouts/MSplash/MSplash.spec.ts
+++ src/components/layouts/MSplash/MSplash.spec.ts
@@ -1,6 +1,6 @@
 import { testComponentFactory } from '@/utils/testComponentFactory';
 import { describe, expect, it } from 'vitest';
-import MSplashVue from './MSplash.vue';
+import MSplashVue from '@/components/layouts/MSplash/MSplash.vue';
 
 describe('Layouts: MSplash', () => {
 	it('should render the component correctly', () => {
