39c8a3524541572ab020f88f21a97b1dfb132312
Mario Bandeira <josemariobandeira@hotmail.com>
2024-03-02 10:14:17 -0300

    feat: MAvatar

---
+++ src/components/shared/MAvatar/MAvatar.vue
@@ -0,0 +1,45 @@
+<template>
+	<img :src="filepath" :alt="altText" :class="['m-avatar', `-${size}`]" />
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+	name: 'm-avatar'
+});
+
+defineProps({
+	filepath: {
+		type: String,
+		required: true
+	},
+	size: {
+		type: String,
+		default: 'small'
+	},
+	altText: {
+		type: String,
+		default: 'avatar image'
+	}
+});
+</script>
+
+<style lang="scss">
+.m-avatar {
+	border-radius: 50%;
+
+	&.-small {
+		width: 50px;
+		height: 50px;
+	}
+
+	&.-medium {
+		width: 80px;
+		height: 80px;
+	}
+
+	&.-large {
+		width: 100px;
+		height: 100px;
+	}
+}
+</style>
